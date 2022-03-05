aa16a961a09c712749ba50977a2810e2d0780169
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-03-05 21:02:41 +0000

    two balls

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ from rendering.scene_objects import primitives, camera, lighting
 
 root = Scene()
 
-light = lighting.PointLight(root, vector.Vector3(3, 3, 3), 1)
-camera = camera.Camera(root, vector.Vector3(0, 0, 4), (1080, 720))
+light = lighting.PointLight(root, vector.Vector3(0, 4, 4), 2)
+cam = camera.Camera(root, vector.Vector3(0, 0, 4), (108, 72))
 
-sphere = primitives.Sphere(root, vector.Vector3(0, 0, 0), 1)
-r, dist = sphere.bounce_specular(ray.Ray(vector.Vector3(0, 0, 2), vector.Vector3(0, 0, -1)))
-print(r.normal)
+sphere1 = primitives.Sphere(root, vector.Vector3(1.25, 0, 0), 1)
+sphere2 = primitives.Sphere(root, vector.Vector3(-1.25, 0, 0), 1)
 
 root.render()
-
-
-
