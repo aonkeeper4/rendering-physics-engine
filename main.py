8dd17d431c4674035adfab0b0807fc5aa77ae23d
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-03-05 20:32:31 +0000

    did a 1080x720 render it looks nice

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from rendering.scene_objects import primitives, camera, lighting
 root = Scene()
 
 light = lighting.PointLight(root, vector.Vector3(3, 3, 3), 1)
-camera = camera.Camera(root, vector.Vector3(0, 0, 4), (256, 144))
+camera = camera.Camera(root, vector.Vector3(0, 0, 4), (1080, 720))
 
 sphere = primitives.Sphere(root, vector.Vector3(0, 0, 0), 1)
 r, dist = sphere.bounce_specular(ray.Ray(vector.Vector3(0, 0, 2), vector.Vector3(0, 0, -1)))
