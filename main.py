556de3d42b46cde4012bc3e9075652ecf0837336
Dylan Strudwick <72671213+n0rmaliz@users.noreply.github.com>
2022-03-05 21:04:45 +0000

    two balls part 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rendering.scene_objects import primitives, camera, lighting
 
 root = Scene()
 
-light = lighting.PointLight(root, vector.Vector3(0, 4, 4), 2)
+light = lighting.PointLight(root, vector.Vector3(2, 4, 4), 2)
 cam = camera.Camera(root, vector.Vector3(0, 0, 4), (108, 72))
 
 sphere1 = primitives.Sphere(root, vector.Vector3(1.25, 0, 0), 1)
