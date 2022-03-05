eb8c518598478be7f978b9199f0fa89f416029d4
n0rmaliz <dylanstrudwick1226@gmail.com>
2022-03-05 21:11:25 +0000

    help this is a bit of a mess

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from rendering.scene_objects import primitives, camera, lighting
 
 root = Scene()
 
-<<<<<<< HEAD
-light = lighting.PointLight(root, vector.Vector3(0, 4, 4), 2)
-=======
 light = lighting.PointLight(root, vector.Vector3(2, 4, 4), 2)
->>>>>>> 556de3d42b46cde4012bc3e9075652ecf0837336
 cam = camera.Camera(root, vector.Vector3(0, 0, 4), (108, 72))
 
 sphere1 = primitives.Sphere(root, vector.Vector3(1.25, 0, 0), 1)
